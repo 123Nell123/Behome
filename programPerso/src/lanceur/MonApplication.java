55007a77e41ab2d154ec82cbd9a8eb5e4c0a2b63
Nell <nell.pied@yahoo.fr>
2018-08-27 22:38:13 +0200

    ajout de BD

--- programPerso/src/lanceur/MonApplication.java
+++ programPerso/src/lanceur/MonApplication.java
@@ -8,9 +8,29 @@ import domaine.Client;
 import domaine.MaxiAgent;
 import domaine.Propriete;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
+
+
 public class MonApplication {
 
 	public static void main(String[] args) {
+		
+		sauverEnBase( 1,"Daudet","Alphonse",01,01);
+		System.out.println("ca commence");
+		lireEnBase();
+		
+		
+		
+		
+		
 		// TODO Auto-generated method stub
 
 		// declaration prealable
@@ -194,7 +214,7 @@ public class MonApplication {
 
 					case 4:
 
-						// TODO afficher la liste d'agent dispo cad qui ont unnbre de client <10;
+						// TODO afficher la liste d'agent dispo cad qui ont un nbre de client <10;
 						System.out.println("******************************************************");
 						System.out.println(" il s'agit de retrouver l'effectif chez Behome  /n");
 						ArrayList<Agent> effectif = new ArrayList<Agent>();
@@ -249,4 +269,102 @@ public class MonApplication {
 		 */
 
 	}
+	
+	
+	public static void sauverEnBase(int idAgent, String nom, String prenom, int nbBien, int IdClient) {
+
+		// Information d'accès à la base de données
+		String url = "jdbc:mysql://localhost/behome?autoReconnect=true&useSSL=false";
+		String login = "root";
+		String passwd = "";
+		Connection cn =null;
+		Statement st =null;
+
+		try {
+
+			// Etape 1 : Chargement du driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// Etape 2 : récupération de la connexion
+			cn = DriverManager.getConnection(url, login, passwd);
+
+			// Etape 3 : Création d'un statement
+			st = cn.createStatement();
+
+			String sql = "INSERT INTO `agent` (`idAgent`,`nom`,`prenom`,`nbBien`,`idClient`) VALUES ('"
+					+ idAgent + "','"+ nom +"','"+prenom+"','"+nbBien+','+IdClient+"')";
+
+			
+			
+			
+			// Etape 4 : exécution requête
+			st.executeUpdate(sql);
+
+			// Si récup données alors étapes 5 (parcours Resultset)
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			try {
+			// Etape 6 : libérer ressources de la mémoire.
+				cn.close();
+				st.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void lireEnBase() {
+
+		// Information d'accès à la base de données
+		String url = "jdbc:mysql://localhost/behome?autoReconnect=true&useSSL=false";
+		String login = "root";
+		String passwd = "";
+		Connection cn =null;
+		Statement st =null;
+		ResultSet rs =null;
+		
+		try {
+
+			// Etape 1 : Chargement du driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// Etape 2 : récupération de la connexion
+			cn = DriverManager.getConnection(url, login, passwd);
+
+			// Etape 3 : Création d'un statement
+			st = cn.createStatement();
+
+			String sql = "SELECT * FROM agent";
+
+			// Etape 4 : exécution requête
+			rs = st.executeQuery(sql);
+
+			// Si récup données alors étapes 5 (parcours Resultset)
+
+			while (rs.next()) {
+				
+				System.out.println(rs.getString("idAgent")+ rs.getString("nom")); 
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+			// Etape 6 : libérer ressources de la mémoire.
+				cn.close();
+				st.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	
+	}
+	
+	
 }
